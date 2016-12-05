9d856bf574e7ebdcd1722d7997b348eec0ca9079
overl <paul.sav21@gmail.com>
2016-12-06 01:12:34 +0400

    Добавил качество оценки ряда, метод getEvaluationQuality

--- src/Model/Solution.java
+++ src/Model/Solution.java
@@ -17,7 +17,7 @@ public class Solution {
     private List<Point> solution;
     BaseController controller;
 
-    public double getSum(double x, double t, int N,double k,double c,double R,double Uenv) {
+    public static double getSum(double x, double t, int N,double k,double c,double R,double Uenv) {
         double a2 = k / c;
         //double eps = 0.00001;
         double res = 0;
@@ -29,10 +29,17 @@ public class Solution {
     public void calculateSolution(double t,double alpha,double c,double R,double k, double Uenv,double eps) {
         solution = new ArrayList<Point>();
         int N;
-        if(t==0)
-            N = 500;//getEvaluation(eps,1,R,k,c);
-        else
-            N = getEvaluation(eps,t,R,k,c);
+        int n;
+        if(t==0) {
+            n = 500;//getEvaluation(eps,1,R,k,c);
+            N = getEvaluationQuality(n,alpha,R,c,Uenv,t,k,eps);
+        }
+        else {
+            n = getEvaluation(eps, t, R, k, c);
+            N = getEvaluationQuality(n,alpha,R,c,Uenv,t,k,eps);
+            System.out.println(n);
+            System.out.println(N);
+        }
         //double a2 = Params.k/Params.c;
         double b2 =alpha * 2 / (c * R);
         double i = Math.PI * R;
@@ -64,5 +71,24 @@ public class Solution {
         return N;
     }
 
-    ;
+    public static int getEvaluationQuality(int N,double alpha, double R, double c, double Uenv, double t, double k, double eps){
+        int n = N;
+        double b2 =alpha * 2 / (c * R);
+        double i = Math.PI * R;
+        double U=0;
+        double temp=0;
+        double Usub = 0;
+        double step = 2 * Math.PI * R / Coeffs.pointNumber;
+        for (double j = -i; j <= i; j += step) {
+            U = getSum(j, t, N, k, c, R, Uenv);
+        }
+        do {
+            n--;
+            for (double j = -i; j <= i; j += step) {
+                temp = getSum(j, t, n, k, c, R, Uenv);
+            }
+            Usub += U-temp;
+        } while(abs(Usub)<eps);
+        return n;
+    }
 }
