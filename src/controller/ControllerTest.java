548d1009dffc98daecec0fcc958461a5ba0be138
Александр <Le_barto@mail.ru>
2017-04-16 21:57:51 +0400

    пошел ты

--- src/controller/ControllerTest.java
+++ src/controller/ControllerTest.java
@@ -14,7 +14,8 @@ public class ControllerTest {
     @Test
     public void updatePoints() throws Exception {
 
-        List<Double> eps = new ArrayList<>();
+        List<Double> epsDel = new ArrayList<>();
+        List<Double> epsNedel = new ArrayList<>();
         Controller controller = new Controller(new BaseMainView() {
             @Override
             public void createGraph(List<Point> points) {
@@ -23,19 +24,19 @@ public class ControllerTest {
 
             @Override
             public void updateGraph(List<Point> points, List<Point> explit, List<Point> implict) {
-                if (eps.size() == 0) {
+                if (epsNedel.size() == 0) {
                     for (int i = 0; i < points.size(); i++) {
-                        System.out.println(points.get(i).getY() - explit.get(i).getY());
-                        eps.add(points.get(i).getY() - explit.get(i).getY());
+                        System.out.println("epsDel = "+(points.get(i).getY() - explit.get(i).getY()));
+                        epsNedel.add(points.get(i).getY() - explit.get(i).getY());
                     }
                 }
                 else{
                     for (int i = 0; i < points.size(); i++) {
                         //System.out.println(points.get(i).getY() - explit.get(i).getY());
-                        System.out.println(eps.get(i)/(points.get(i).getY() - explit.get(i).getY()));
+                        System.out.println("epsNedel = "+(points.get(i).getY() - explit.get(i).getY()));
+                        epsDel.add(points.get(i).getY() - explit.get(i).getY());
                     }
                 }
-
             }
 
             @Override
@@ -43,8 +44,8 @@ public class ControllerTest {
 
             }
         });
-        controller.updatePoints(100, 0.005, 1.84, 25, 0.065, 0, 0.0001, false, 20, 8, true);
         controller.updatePoints(100, 0.005, 1.84, 25, 0.065, 0, 0.0001, false, 20, 8, false);
+        controller.updatePoints(100, 0.005, 1.84, 25, 0.065, 0, 0.0001, false, 20, 8, true);
     }
 
 }
